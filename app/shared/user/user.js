ef28b9c31883d8aca85c825e4e3f100f6c9d7dc9
William <dwy666@hotmail.com>
2017-09-14 15:32:35 +0800

    remove js

--- app/shared/user/user.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var User = (function () {
-    function User() {
-    }
-    return User;
-}());
-exports.User = User;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidXNlci5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbInVzZXIudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFBQTtJQUFBO0lBR0EsQ0FBQztJQUFELFdBQUM7QUFBRCxDQUFDLEFBSEQsSUFHQztBQUhZLG9CQUFJIiwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGNsYXNzIFVzZXJ7XHJcbiAgZW1haWw6c3RyaW5nO1xyXG4gIHBhc3N3b3JkOnN0cmluZztcclxufSJdfQ==
